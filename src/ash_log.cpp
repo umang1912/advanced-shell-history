f061bbedcc39baaeac514aa636b76b00b754794e
riddle <riddle@fc996830-f55c-26f1-6082-e4b8d94fb6a9>
2011-03-13 07:32:05 +0000

    Added flag processing for -s --get-session-id flag via getopt_long.

--- src/ash_log.cpp
+++ src/ash_log.cpp
@@ -1,8 +1,38 @@
 #include <getopt.h>
+#include <iostream>  /* for cout, endl */
+#include <stdlib.h>  /* for exit */
+
 #include "sqlite3.h"
 
 
+using namespace std;
+
+
+void get_session_id() {
+  // TODO(cpa): open the database
+  // TODO(cpa): if the current session_id exists, emit it
+  // TODO(cpa): else create a new session_id and emit it
+  // TODO(cpa): close the database
+  exit(0);
+}
+
+
+static struct option options[] = {
+  {"get-session-id", 0, 0, 's'}
+};
+
+
 int main(int argc, char ** argv) {
-  // TODO(cpa): check for the -s argument, return the current session ID
+  int c = 0;
+  while (c != -1) {
+    int index = 0;
+    c = getopt_long(argc, argv, "s", options, &index);
+    switch (c) {
+      case 's':
+        get_session_id();
+      default:
+        cout << "AWESOME: " << c << endl;
+    }
+  }
   return 0;
 }
